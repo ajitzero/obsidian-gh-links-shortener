c1586441ba6262271c51fb5d73a84bbe81b693d0
Ajit Panigrahi <ajitzero@gmail.com>
2026-07-01 09:08:34 +0530

    feat: support issue comments and more link types

--- main.ts
+++ main.ts
@@ -35,19 +35,57 @@ function formatGHLink(pastedText: string): string | null {
 	if (url.hostname != 'github.com') {
 		return null;
 	}
-	if (url.hash) {
-		// Don't replace with simple reference if there's a hash like "#issuecomment-492445254".
-		// TODO: Replace those but with a "(comment)" suffix?
+
+	const details = parseDetails(url.pathname);
+	if (!details) {
 		return null;
 	}
 
-	const pullOrIssueRegex = /^\/([^/]+)\/([^/]+)\/(issues|pull)\/(\d+)\/?$/;
-	const match = pullOrIssueRegex.exec(url.pathname);
-	if (match) {
-		const [, owner, repo, , number] = match;
-		return `[${owner}/${repo}#${number}](${pastedText})`;
-	}
+	const { owner, repo, type, id } = details;
+	const idPrefix = type === 'commit' ? '@' : '#';
+	const idFormat = type === 'commit' ? id.slice(0, 7) : id;
+	const suffix = url.hash && isCommentHash(url.hash) ? ' (comment)' : '';
+	return `[${owner}/${repo}${idPrefix}${idFormat}${suffix}](${pastedText})`;
+}
 
-	// Didn't match any path pattern. Ignore.
-	return null;
+/**
+ * Tries to parse properties if this is a valid URL.
+ *
+ * Test cases:
+ * Case 1:
+ * - https://github.com/owner/repo/issues/{issue-number} -> owner/repo#{issue-number}
+ * - https://github.com/owner/repo/pull/{pr-id} -> owner/repo#{pr-id}
+ * - https://github.com/owner/repo/discussions/{discussion-id} -> owner/repo#{discussion-id}
+ *
+ * Case 2:
+ * - https://github.com/owner/repo/commit/{commit-sha} -> owner/repo@{commit-sha, first 7 characters only}
+ */
+function parseDetails(pathname: URL['pathname']) {
+	const regex = /^\/([^/]+)\/([^/]+)\/(issues|pull|discussions|commit)\/([^/]+)\/?$/;
+	const match = regex.exec(pathname);
+	if (!match) {
+		return null;
+	}
+	const [, owner, repo, type, id] = match;
+	return { owner, repo, type, id };
+}
+
+/**
+ * Checks if the hash is a comment hash.
+ *
+ * Test cases:
+ * - #issue-{number}
+ * - #issuecomment-{number}
+ * - #pullrequestreview-{number}
+ * - #review-{number}
+ * - #discussion-{number}
+ * - #discussion_r{number} (no hyphen)
+ * - #discussioncomment-{number}
+ *
+ * Other factors:
+ * - Optional hyphen in some cases.
+ * - Optional trailing slash in some cases.
+ */
+function isCommentHash(hash: URL['hash']): boolean {
+	return /^#(?:issue(?:comment)?|discussion(?:comment|_r)?|(?:pullrequest)?review)-?\d+\/?$/.test(hash || '');
 }
